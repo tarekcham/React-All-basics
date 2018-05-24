9042bc16ccf283ac1c8252a18da2ef444019f59c
tarekcham <tarek.alkhatib1@gmail.com>
2018-05-24 08:20:32 +0200

    all react concetps

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+import classes from "./Person.css";
+import withClass from "../../../hoc/withClass";
+import Auxiliary from "../../../hoc/Auxiliary";
+import { AuthContext } from '../../../containers/App';
+
+class Person extends Component {
+	constructor(props) {
+		super(props);
+		console.log("[Person.js] Inside Constructor", props);
+		this.inputElement = React.createRef();
+	}
+
+	componentWillMount() {
+		console.log("[Person.js] Inside componentWillMount()");
+	}
+
+	componentDidMount() {
+		console.log("[Person.js] Inside componentDidMount()");
+		if(this.props.position === 0 )
+		this.inputElement.current.focus();
+	}
+
+	render() {
+		console.log("[Person.js] Inside render()");
+		return (
+			<Auxiliary>
+				<AuthContext.Consumer>
+					{ddd=> ddd ? <p>I'm authenticated</p> : null }
+				</AuthContext.Consumer>
+				<p onClick={this.props.click}>
+					I'm {this.props.name} and I am {this.props.age} years old!
+				</p>
+				<p>{this.props.children}</p>
+				<input
+					ref={this.inputElement}
+					type="text"
+					onChange={this.props.changed}
+					value={this.props.name}
+				/>
+			</Auxiliary>
+		);
+		// return [
+		//     <p key="1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>,
+		//     <p key="2">{this.props.children}</p>,
+		//     <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
+		// ]
+	}
+}
+
+export default withClass(Person, classes.Person);
